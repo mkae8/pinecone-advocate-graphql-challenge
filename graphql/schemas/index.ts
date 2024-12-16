6166b25d5b43fa81c5036fe209ff470db39c80f0
24LP9087 24LP9087 <24LP9087@admins-iMac.local>
2024-12-16 12:57:02 +0800

    coding 1e Temuujin.B

--- graphql/schemas/index.ts
+++ graphql/schemas/index.ts
@@ -1,7 +1,7 @@
 import { gql } from "graphql-tag";
 
 export const typeDefs = gql`
-  type Query {
+  type Query {git a
     getAllTasks: [Task]
     helloQuery: String
     getDoneTaskLists: [Task]
